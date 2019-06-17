dc8934d06713405046aeb037912ed635f8cef9a7
em <44924704+emmamacintosh@users.noreply.github.com>
2019-06-17 03:18:19 +0200

    refactoring state.js

--- game-practice/src/components/Snake/Snake2/state.js
+++ game-practice/src/components/Snake/Snake2/state.js
@@ -3,32 +3,14 @@ import { randomInt } from './helpers'
 
 const NUMBER_OF_ROWS = 20
 const NUMBER_OF_COLS = 20
+const TYPES = {
+  SNAKE: "SNAKE"
+}
 
 class GameState extends React.Component {
   constructor(props) {
     super(props)
-    this.grid = this.initGrid()
     this.snake = new Snake()
-    //this.fillGrid()
-  }
-
-  initGrid() {
-    return new Array(NUMBER_OF_ROWS).fill(new Array(NUMBER_OF_COLS).fill(0))
-  }
-
-  fillGrid() {
-    for(let i = 0; i <= this.snake.length; i++) {
-      this.grid[this.snake.head.x][this.snake.head.y - i] = 1
-    }
-  }
-
-  isSnake(x, y) {
-    return this.grid[x][y] === 1
-  }
-
-  onHeadMoved(oldPos, newPos) {
-    //this.grid[oldPos.x - this.snake.length][oldPos.y - this.snake.length] = 0
-    this.grid[newPos.x][newPos.y] = 1
   }
 
   render () {
@@ -41,49 +23,20 @@ class GameState extends React.Component {
 class Snake extends Component {
   constructor(props) {
     super(props)
-    if(this.props) {
-      this.head = this.props.head
-      this.length = this.props.length
-    }
-    if(this.props === undefined){
-      this.head = this.randomPlace()
-      this.length = 3
-    }
-    this.tail = this.initTail()
-  }
-
-  initTail() {
-    let tail = []
-    for (let i = 1; i <= this.length; i++) {
-      tail.push({x: this.head.x - i, y: this.head.y})
-    }
-    return tail
-  }
-
-  randomPlace() {
-    return {
-      x: randomInt(0, NUMBER_OF_ROWS),
-      y: randomInt(0, NUMBER_OF_COLS)
-    }
-  }
-
-  setHead(x, y) {
-    const oldHead = this.head
-    this.head = {x, y}
-  }
-
-  isSnake({ x, y }) {
-    return this.tail.concat([this.head]).some(el => {
-      return el.x === x && el.y === y
-    })
+    this.body = [[13, 10], [12, 10], [11, 10], [10, 10]]
+    this.direction = [0,1]
+    this.type = TYPES.SNAKE
   }
 
   move() {
-    this.setHead(this.head.x, this.head.y + 1)
+    return Object.assign({}, this)
   }
 }
 
 module.exports = {
   GameState,
-  Snake
+  Snake,
+  NUMBER_OF_ROWS,
+  NUMBER_OF_COLS,
+  TYPES
 }
--- game-practice/src/components/tests/state.test.js
+++ game-practice/src/components/tests/state.test.js
@@ -1,35 +1,69 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import GameState from '../Snake/Snake2/state'
+import { GameState, Snake } from '../Snake/Snake2/state'
 
 describe('Initial Game State', () => {
   let gameState = {}
 
   beforeEach(() => {
-    gameState = new GameState
+    gameState = new GameState()
   })
-  it('inital grid has 20 rows and 20 cols', () => {
-    const grid = gameState.grid
 
-    expect(grid.length).toBe(20)
-    expect(grid[0].length).toBe(20)
-  }),
-  it('inital grid is initialized with 0', () => {
-    const grid = gameState.grid
-    const gridZeroed = grid[0].every(cell => cell === 0|| cell === 1)
-    expect(gridZeroed).toBeTruthy()
-  }),
-  it('snake random position', () => {
-    const snake = gameState.snake
-    
-    expect(snake.head.x).toBeGreaterThanOrEqual(0)
-    expect(snake.head.y).toBeGreaterThanOrEqual(0)
+  describe('Grid', () => {
+    it('initial grid has 20 rows and 20 cols', () => {
+      const grid = gameState.grid
+  
+      expect(grid.length).toBe(20)
+      expect(grid[0].length).toBe(20)
+    }),
+    it('initial grid is initialized with 0', () => {
+      const grid = gameState.grid
+      
+      const gridZeroed = grid.every(row => row.every(cell => cell !== undefined))
+      expect(gridZeroed).toBeTruthy()
+    })
   })
-  it('snake is placed on the grid', () => {
-    const gamestate = gameState
-    const snake = gamestate.snake
-    const grid = gamestate.grid
-    
-    expect(grid[snake.head.x][snake.head.y]).toBe(1)
+ 
+  describe('Snake', () => {
+    it('random position is assigned', () => {
+      const snake = gameState.snake
+      
+      expect(snake.head.x).toBeGreaterThanOrEqual(0)
+      expect(snake.head.y).toBeGreaterThanOrEqual(0)
+    })
+    it('is placed on the grid', () => {
+      const { snake } = gameState
+
+      expect(gameState.isSnake(snake.head.x, snake.head.y)).toBeTruthy
+    })
+    it('has 3 cells long tail', () => {
+      const snake = new Snake({length: 4, head: {x: 5, y: 5}})
+      console.log(snake.tail)
+
+      expect(snake.tail.length).toBe(4)
+      expect(snake.tail[0].x).toBe(4)
+      expect(snake.tail[1].x).toBe(3)
+      expect(snake.tail[2].x).toBe(2)
+      expect(snake.tail[0].y).toBe(5)
+    })
+  }),
+  describe.only('isState', () => {
+    it('is truthy when asked about tail', () => {
+      const snake = new Snake({length: 3, head: {x: 3, y: 3}})
+      expect(snake.isSnake({x: 0, y: 3})).toBeTruthy()
+    })
+  }),
+  describe('Movements', () => {
+    it('can move one cell', () => {
+      const { snake } = gameState
+      snake.setHead(5, 5)
+
+      console.log("before: ", snake.head)
+      snake.move()
+      console.log("after: ", snake.head)
+      expect(snake.head.x).toBe(5)
+      expect(snake.head.y).toBe(6)
+  
+    })
   })
 });
